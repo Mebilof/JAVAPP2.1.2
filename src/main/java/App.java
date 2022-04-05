6ce3f464090f325dd07b67d8ab2d231617833630
Mebilof <cuchik.rasdon@gmail.com>
2022-04-05 16:53:47 +0300

    Spring Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,11 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1 == bean2);
+        Cat bean11 = (Cat) applicationContext.getBean("cat");
+        Cat bean12 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean11 == bean12);
     }
 }
